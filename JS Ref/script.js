3af0810c8988ab2e4d561d5c220eeb2df3cd8f94
onyachmark <onyachmark@gmail.com>
2022-01-03 05:12:17 +0300

    changes to script file

--- JS Ref/script.js
+++ JS Ref/script.js
@@ -6,23 +6,35 @@
 let countEl = document.getElementById('count-el')
 let count = 0
 function increment(){
-    count = count + 1
+    count += 1
     countEl.innerText = count
 }
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
+let saveEl = document.getElementById('save-el')
+console.log(saveEl);
 
 // 1. Create a function, save(), which logs out the count when it's called
 function save(){
-    count = count
-    // console.log(count);
+// 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    let saved = `${count} - `
+// 3. Render the variable in the saveEl using innerText
+    saveEl.innerText = saved
 }
 
 //working with strings
+
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
 let welcomeEl = document.getElementById('welcome-el')
 console.log(welcomeEl);
 
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
 let hisName = 'Marrk'
-let greeting = 'Hello'
+let greeting = 'Welcome back'
+
+// Render the welcome message using welcomeEl.innerText
 welcomeEl.innerText = `${greeting} ${hisName},
 I suppose you're well this morning!
 `
+welcomeEl.innerText += '👋'
 
